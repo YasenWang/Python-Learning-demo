b1db70f44032c49bba8a7ec499381e43da1ab79e
YasenWang <00000000@wangyusheng.cn>
2019-02-27 21:34:26 +0800

    文本进度条

---
+++ ProgramControl/CalculatePi.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+# _author_ = ' Yasen '
+#==========================
+from random import random
+from time import perf_counter
+
+def CalculatePiV1():
+    pi = 0
+    N = 100
+    for k in range(N):
+        pi += 1/pow(16, k)*\
+              (4/(8*k + 1) - 2/(8*k + 4) -
+               1/(8*k + 5) - 1/(8*k + 6))
+    print("圆周率值是: {}".format(pi))
+
+def MonteCarloCalculate():
+    DARTS = 3000**2
+    hits = 0.0
+    start = perf_counter()
+    for i in range(1, DARTS+1):
+        x, y = random(), random()
+        dist = pow(x**2 + y**2, 0.5)
+        if dist <= 1.0:
+            hits += 1
+    pi = 4 * (hits/DARTS)
+    print("圆周率的值是: {}".format(pi))
+    print("运行时间是: {:.5f}s".format(perf_counter()-start))
+
+#==========================
+if __name__ == '__main__':
+    # CalculatePiV1()
+    MonteCarloCalculate()
--- SimpleDataType/TextProgressBar.py
+++ SimpleDataType/TextProgressBar.py
@@ -14,7 +14,26 @@ def SimpleTextProgressBar():
         t.sleep(0.1)
     print("======执行结束======")
 
+def TextProgressBarPro():
+    for i in range(101):
+        print("\r{:3}%".format(i), end="")
+        t.sleep(0.1)
+
+def FullTextProgressBar():
+    scale = 50
+    print("执行开始".center(scale//2,"-"))
+    start = t.perf_counter()
+    for i in range(scale + 1):
+        a = '*' * i
+        b = '·' * (scale - i)
+        c = (i / scale) * 100
+        dur = t.perf_counter() - start
+        print("\r{:^3.0f}%[{}->{}]{:.2f}s".format(c, a, b, dur), end="")
+        t.sleep(0.1)
+    print("\n"+"执行结束".center(scale//2,"-"))
 #==========================
 if __name__ == '__main__':
     #pass
-    SimpleTextProgressBar()
+    #SimpleTextProgressBar()
+    #TextProgressBarPro()
+    FullTextProgressBar()
